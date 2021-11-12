15c93d59c173f55830a5ed09964fcb8015da7a6e
Ocean Winckler <oceanwinckler.rem@gmail.com>
2021-11-12 03:20:13 +0100

    V0.1 \n+Default Design

--- src/index.js
+++ src/index.js
@@ -1,11 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./assets/app.css";
+import "./assets/utils.css";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			activeModule: "dashboard"
+		}
+
+		this.updateState	= this.updateState.bind(this);
+	}
+
+	updateState(target, value) {
+		this.setState({
+			[target]: value
+		});
+	}
+
+	render() {
+		return(
+			<React.StrictMode>
+				<div className="module grid-layout _2-grid">
+
+					<div className="content">
+						<div className="header">
+							<div className="title">All Items</div>
+							<span className="material-icons">add</span>
+						</div>
+						<div className="body">
+							<input
+								className="text-field"
+								placeholder="Search..."
+							/>
+						</div>
+					</div>
+
+					<div className="content">
+						<div className="header">
+							<div className="title">Selected Items</div>
+							<span className="material-icons">save</span>
+						</div>
+					</div>
+				</div>
+			</React.StrictMode>
+		)
+	}
+}
+
+render(<App />, document.getElementById("app"));
